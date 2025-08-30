1173e26cb71c0c02287b06c31bee19619de36289
YogeshVenugopal <yogeshvenugopal875@gmail.com>
2025-08-30 13:00:57 +0530

    Remove Unwanted things

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Login from './pages/Login'
 import Template from './pages/Template'
 
 const App = () => {
--- Server/Routes/record.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { createRecord, getRecord } from '../Controllers/record.controller.js';
-
-const recordRoutes = express.Router();
-
-recordRoutes.post('/create-record', createRecord);
-recordRoutes.get('/get-record/:recordId', getRecord);
-
-export default recordRoutes;
